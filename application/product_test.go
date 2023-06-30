b5a644623dbd5c3a098f238717ee3a4e90ebab60
Pedro Henrique <pedro.souza@mova.vc>
2023-06-30 08:35:13 -0300

    setup project

--- application/product_test.go
+++ application/product_test.go
@@ -1,59 +1,52 @@
 package application_test
 
-import (
-	"github.com/codeedu/go-hexagonal/application"
-	uuid "github.com/satori/go.uuid"
-	"github.com/stretchr/testify/require"
+import( 
 	"testing"
+	"github.com/streechr/testify/require"
 )
 
-func TestProduct_Enable(t *testing.T) {
+func TestProduct_Enable(t *testing.T){
 	product := application.Product{}
 	product.Name = "Hello"
 	product.Status = application.DISABLED
 	product.Price = 10
 
-	err := product.Enable()
+	err:= product.Enable()
 	require.Nil(t, err)
-
+	
 	product.Price = 0
-	err = product.Enable()
-	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+	err:= product.Enable()
+	require.Equal(t, "Invalid price; Must be greather than zero", err.Error)
 }
 
-func TestProduct_Disable(t *testing.T) {
+
+func TestProduct_Disable(t *testing.T){
 	product := application.Product{}
 	product.Name = "Hello"
 	product.Status = application.ENABLED
-	product.Price = 0
-
-	err := product.Disable()
-	require.Nil(t, err)
-
 	product.Price = 10
-	err = product.Disable()
-	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+
+	err:= product.Disable()
+	require.Equal(t, "Invalid price; Must be zero", err.Error)
+
+	
+	product.Price = 0
+	err:= product.Disable()
+	require.Nil(t, err)	
 }
 
-func TestProduct_IsValid(t *testing.T) {
-	product := application.Product{}
+func TestProduct_IsValid(t *testing.T){
+	product:= application.Product{}
+	product.Name = "Hello"
 	product.ID = uuid.NewV4().String()
-	product.Name = "hello"
-	product.Status = application.DISABLED
+	product.Status = application.ENABLED
 	product.Price = 10
 
-	_, err := product.IsValid()
-	require.Nil(t, err)
+	_, err =: product.IsValid()
+	required.Nil(t, err)
 
 	product.Status = "INVALID"
-	_, err = product.IsValid()
-	require.Equal(t, "the status must be enabled or disabled", err.Error())
+	_, err =: product.IsValid()
+	require.Equal(t, "Invalid status", err.Error)
 
-	product.Status = application.ENABLED
-	_, err = product.IsValid()
-	require.Nil(t, err)
-
-	product.Price = -10
-	_, err = product.IsValid()
-	require.Equal(t, "the price must be greater or equal zero", err.Error())
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/codeedu/go-hexagonal
+module github.com/codeedu/fc2-arquitetura-hexagonal
 
 go 1.16
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/codegangsta/negroni v1.0.0
 	github.com/golang/mock v1.5.0
 	github.com/gorilla/mux v1.8.0
